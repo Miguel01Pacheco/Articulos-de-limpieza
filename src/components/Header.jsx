72b9e7efd46be9590c0a6fda77cf937e6baf30bd
Miguel01Pacheco <miguemap01@gmail.com>
2024-08-13 20:13:41 -0300

    nuevos colores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,15 @@ function Header() {
   return (
     <header className="header">
       <div className='titulo'>
-        <h1>MAAS</h1>
-        <h2>Artículos de Limpieza</h2>
+      <img src="/logo-mass.jpeg" alt="Logo" className="logo-mass" />
       </div>
       <nav>
         <Link to="/">Inicio</Link>
         <Link to="/cart">
-          <div className='link-carrito'>
+          
           Ver Carrito
           <img src="/icono-carrito.png" alt="Cart" className="cart-icon" />
-          </div>
+       
 
         </Link>
       </nav>
