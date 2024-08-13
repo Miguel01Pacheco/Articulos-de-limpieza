8d0863de6e64a0c54f7a89b97c17539dee0f0295
Miguel01Pacheco <miguemap01@gmail.com>
2024-08-12 23:06:06 -0300

    imagenes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
         <Link to="/cart">
           <div className='link-carrito'>
           Ver Carrito
-          <img src="/src/assets/images/icono-carrito.png" alt="Cart" className="cart-icon" />
+          <img src="/public/icono-carrito.png" alt="Cart" className="cart-icon" />
           </div>
 
         </Link>
